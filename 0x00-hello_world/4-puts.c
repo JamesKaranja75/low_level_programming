429eaa4f1d3329a9bc0edddfc2e5d51e6bc7d75b
JamesKaranja75 <karanjajamess101@gmail.com>
2023-08-10 07:10:40 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+
 /**
- * main - 4-puts.c
- * description - Write a C program that prints exactly
+ * main - Entry point
+ *
+ * Description: Prints a specific message to the console.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
+    puts("\"Programming is like building a multilingual puzzle");
 
-        return (0);
+    return (0);
 }
+
