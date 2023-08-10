039e6c7b0770ca4018131dbb356db0b64948f8bd
JamesKaranja75 <karanjajamess101@gmail.com>
2023-08-10 04:57:34 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/** 
+ * main entry point
+ * return 0 always success
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return(0);
+}
