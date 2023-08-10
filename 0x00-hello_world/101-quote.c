416a64617422a87d80aaf10bee5da136177a1049
JamesKaranja75 <karanjajamess101@gmail.com>
2023-08-10 07:42:55 -0700

    Simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
 #include <unistd.h>
+
 /**
- * main - 101-qoute.c
- * description - Write a C program that prints exactly and that piece of
- * art is useful\"
- *- Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Description: Prints a specific message to the standard error.
+ *
+ * Return: Always 1 (Failure)
  */
 int main(void)
 {
-        write(2,
-              "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-              59);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-        return (1);
+return (1);
 }
+
