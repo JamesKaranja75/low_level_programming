866bb0366b6216e566adc3f9c23749f067fa6a87
JamesKaranja75 <karanjajamess101@gmail.com>
2023-08-17 05:50:53 -0700

    A function that checks for uppercase character

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putcher - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putcher(char c)
+{
+return (write(1, &c, 1));
+}
+
